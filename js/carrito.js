7f1750b6a94a857bca8cebb93c3aea2be773636c
gilbertojgm <gilbertojgm78@gmail.com>
2023-06-13 22:26:39 -0300

    Subiendo Proyecto

---
+++ js/carrito.js
@@ -0,0 +1,115 @@
+let productoEnCarrito = localStorage.getItem("producto-en-carrito");
+productoEnCarrito = JSON.parse(productoEnCarrito);
+
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+let btnEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const btnVaciar = document.querySelector("#carrito-acciones-vaciar");
+const contenedorTotal = document.querySelector("#total");
+const btnComprar = document.querySelector("#carrito-acciones-comprar");
+
+function cargarProductosCarrito(){
+
+    if(productoEnCarrito && productoEnCarrito.length > 0){
+    
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoProductos.classList.remove("disabled");
+        contenedorCarritoAcciones.classList.remove("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    
+        contenedorCarritoProductos.innerHTML = "";
+    
+        //mostrar los productos en el html
+        productoEnCarrito.forEach(producto =>{
+    
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="carrito-producto-titulo">
+                    <small>Título</small>
+                    <h3>${producto.categoria.nombre}</h3>
+                </div>
+                <div class="carrito-producto-cantidad">
+                    <small>Cantidad</small>
+                    <p>${producto.cantidad}</p>
+                </div>
+                <div class="carrito-producto-precio">
+                    <small>Precio</small>
+                    <p>$${producto.precio}</p>
+                </div>
+                <div class="carrito-producto-subtotal">
+                    <small>Subtotal</small>
+                    <p>$${producto.precio * producto.cantidad}</p>
+                </div>
+                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+            `;
+    
+            contenedorCarritoProductos.append(div);
+        });
+    
+    }else{
+        contenedorCarritoVacio.classList.remove("disabled");
+        contenedorCarritoProductos.classList.add("disabled");
+        contenedorCarritoAcciones.classList.add("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    }
+    
+    actualizarBtnDelet();
+    actualizarTotal()
+}
+
+cargarProductosCarrito();
+
+//funcion para eliminar productos de carrito
+function actualizarBtnDelet(){
+    btnEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+
+    btnEliminar.forEach(boton =>{
+        boton.addEventListener("click", eliminarDelCarrito);
+    });
+}
+
+function eliminarDelCarrito(e){
+    let idBtn = e.currentTarget.id;
+
+    const index = productoEnCarrito.findIndex(producto => producto.id === idBtn);
+
+    productoEnCarrito.splice(index, 1);
+    cargarProductosCarrito();
+
+    localStorage.setItem("producto-en-carrito", JSON.stringify(productoEnCarrito));
+}
+
+btnVaciar.addEventListener("click", vaciarCarrito);
+
+function vaciarCarrito(){
+    productoEnCarrito.length = 0;
+    localStorage.setItem("producto-en-carrito", JSON.stringify(productoEnCarrito));
+    cargarProductosCarrito();
+}
+
+
+//calcular el total de la compra 
+function actualizarTotal(){
+    const totalCalculado =
+    productoEnCarrito.reduce((acumulador, producto) => acumulador +(producto.precio * producto.cantidad), 0);
+    
+    total.innerText = `$${totalCalculado}`;
+     
+}
+
+//boton compra
+btnComprar.addEventListener("click", comprarCarrito);
+
+function comprarCarrito(){
+    productoEnCarrito.length = 0;
+    localStorage.setItem("producto-en-carrito", JSON.stringify(productoEnCarrito));
+    
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.remove("disabled");
+}
---
+++ js/main.js
@@ -0,0 +1,176 @@
+//Productos
+const productos = [
+    //velas pequeñas
+    {
+        "id": "vela-pequeña",
+        "titulo": "Aroma a Chocolate ",
+        "imagen": "./img/v-chocolate.jpeg",
+        "categoria": {
+            "nombre": "Vela Pequeñas",
+            "id": "vela-pequeña"
+        },
+        "precio": 1500
+    },
+    {
+        "id": "vela-pequeña",
+        "titulo": "Aroma a Fresa ",
+        "imagen": "./img/v-fresa.jpeg",
+        "categoria": {
+            "nombre": "Vela Pequeñas",
+            "id": "vela-pequeña"
+        },
+        "precio": 1500
+    },
+    {
+        "id": "vela-pequeña",
+        "titulo": "Aroma a Jazim ",
+        "imagen": "./img/v-jazmin.jpeg",
+        "categoria": {
+            "nombre": "Vela Pequeñas",
+            "id": "vela-pequeña"
+        },
+        "precio": 1500
+    },
+    //Velas Grandes
+    {
+        "id": "vela-grande",
+        "titulo": "Aroma a Jazim XXL",
+        "imagen": "./img/vg-jazmin.jpeg",
+        "categoria": {
+            "nombre": "Velas Grandes",
+            "id": "vela-grande"
+        },
+        "precio": 5000
+    },
+    //Velas personalizadas
+    {
+        "id": "vela-personalisada",
+        "titulo": "Vela Navideña ",
+        "imagen": "./img/v-navideña.jpeg",
+        "categoria": {
+            "nombre": "Velas Personalizadas",
+            "id": "vela-personalisada"
+        },
+        "precio": 3000
+    },
+    {
+        "id": "vela-personalisada",
+        "titulo": "Vela Personalizada con Aroma a Mocaccino ",
+        "imagen": "./img/vp-moca.jpeg",
+        "categoria": {
+            "nombre": "Velas Personalizadas",
+            "id": "vela-personalisada"
+        },
+        "precio": 3000
+    }
+]
+
+//datos que seran llamadss por el dom
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const btnCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let btnAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+//funcion para mostrar los productos del array en el html
+
+function cargarProductos(productos){
+
+    contenedorProductos.innerHTML = "";
+
+    productos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+
+        div.innerHTML = `   
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Añadir <i class="bi bi-cart-fill"></i></button>
+        `;
+
+        contenedorProductos.append(div); //tiene que ir dentro de la funcion RECORDATORIO
+    })
+
+    actualizarBtnAgg();
+    
+}
+
+cargarProductos(productos);
+
+//Creamos un eventlistener para que los se activen los botones del menu de navegacion
+
+btnCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        //otro foreach para que se active un solo boton
+        btnCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        //usamos un filter para filtrar los productos por categoria
+        if(e.currentTarget.id != "todos"){
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id); //usamos find para que recorra el array y nos traiga el nombre de la categoria seleccionada
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton); //usamos la funcion cargar productos pero pasandole como parametro la constante que se uso para filtrar
+        } else{
+            tituloPrincipal.innerText = "todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+
+//Realizar una funcion para que actualize los botones cada vez que se actualize las muestra de productos
+
+function actualizarBtnAgg(){
+    btnAgregar = document.querySelectorAll(".producto-agregar");
+
+    btnAgregar.forEach(boton =>{
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productoEnCarrito;
+
+let productoEnCarritoLS = localStorage.getItem("producto-en-carrito");
+
+
+if(productoEnCarritoLS){
+    productoEnCarrito = JSON.parse(productoEnCarritoLS);
+    actualizarNumerito();
+}else{
+    productoEnCarrito = [];
+}
+
+
+
+function agregarAlCarrito(e){
+
+    const idBtn = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBtn); //usamos filter para filtrar el producto agregado y asi pushearlo al array de producto en carrito
+
+    //realizamos un if con some para ver si ya existe un producto en el array productoEnCarrito y asi contalibilzar la cantidad del producto que vamos agregar
+    if(productoEnCarrito.some(producto => producto.id === idBtn)){
+        const index = productoEnCarrito.findIndex(producto => producto.id === idBtn);
+        productoEnCarrito[index].cantidad++;
+    } else{
+        productoAgregado.cantidad = 1;
+        productoEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    //guardamos la info en el localStorage para llevarlo al carrito
+    localStorage.setItem("producto-en-carrito", JSON.stringify(productoEnCarrito));
+}
+
+//funcion para que se actualize el numerito del carrito cada vez que agregamos un producto
+function actualizarNumerito(){
+    let nuevoNumerito =  productoEnCarrito.reduce((acumulador, producto) => acumulador + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
